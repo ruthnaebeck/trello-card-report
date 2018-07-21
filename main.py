e751afcc968a8d217601f8c16beeb273ba63020b
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-07-21 18:36:50 -0400

    Working to get Trello card info

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-import authenticate
+import trello
+import requests
+
+def find_zendesk_url(card_id):
+  try:
+    attach_json = requests.request('GET', trello.url_cards + card_id + '/attachments' + trello.tokens).json()
+    if len(attach_json):
+      for a in attach_json:
+        if a['url'].startswith('https://datadog.zendesk.com/agent/tickets/'):
+          return a['url']
+  except:
+    return 'An error occurred'
+  return 'No attachments found'
 
 def main_script():
-  print(authenticate.zendesk_email)
+  for b in trello.trello_boards:
+    for l in b['lists']:
+      list_json = requests.request('GET', trello.url_lists + l['id'] + '/cards' + trello.tokens).json()
+      for c in list_json:
+        l['cards'].append({
+          'name': c['name'],
+          'id': c['id'],
+          'lastUpdated': c['dateLastActivity'],
+          'cardLink': c['shortUrl'],
+          'zendeskLink': find_zendesk_url(c['id'])
+        })
+
+  print(trello.trello_boards)
 
 main_script()
