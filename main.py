959e71b8a610a93af713448a05c36dd22e89582a
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-07-21 22:38:58 -0400

    Added get_zendesk_ticket

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import authenticate
 import trello
 import requests
 import re
@@ -20,19 +21,40 @@ def find_zendesk_url(card):
   except:
     return 'An error occurred'
 
-def main_script():
+def get_zendesk_ticket(ticket_id):
+  try:
+    get_ticket_url = 'https://datadog.zendesk.com/api/v2/tickets/%s.json' % (ticket_id)
+    ticket_json = requests.get(url=get_ticket_url, auth=(authenticate.zendesk_email, authenticate.zendesk_password)).json()['ticket']
+    ticket_parts = {
+      'zAssigneeId': ticket_json['assignee_id'],
+      'zStatus': ticket_json['status'],
+      'zLastUpdated': ticket_json['updated_at']
+    }
+    print(ticket_parts)
+    return ticket_parts
+  except:
+    return 'An error occurred'
+
+def get_trello_cards():
   for b in trello.trello_boards:
     for l in b['lists']:
       list_json = requests.request('GET', trello.url_lists + l['id'] + '/cards' + trello.tokens).json()
       for c in list_json:
+        zendesk_url = find_zendesk_url(c)
+        zendesk_id = zendesk_url.split('/').pop()
+        if len(zendesk_id) <= 6:
+          zendesk_ticket = get_zendesk_ticket(zendesk_id)
         l['cards'].append({
-          'name': c['name'],
+          'tName': c['name'],
           'id': c['id'],
-          'lastUpdated': c['dateLastActivity'],
-          'cardLink': c['shortUrl'],
-          'zendeskLink': find_zendesk_url(c)
+          'tLastUpdated': c['dateLastActivity'],
+          'tUrl': c['shortUrl'],
+          'zUrl': zendesk_url
         })
+  # print(trello.trello_boards)
 
-  print(trello.trello_boards)
+def main_script():
+  get_trello_cards()
+  # get_zendesk_ticket('125226')
 
 main_script()
