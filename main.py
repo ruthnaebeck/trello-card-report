1b1616df9d3d3ecfc93bf6fbc896af15e0058115
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-08-19 19:08:22 -0400

    Leads lookup working

--- main.py
+++ main.py
@@ -129,10 +129,11 @@ def add_to_datadog_api(b, l, a, s):
   try:
     datadog_api[key]['points'] += 1
   except KeyError:
+    lead = get_team_lead(a.split(':')[1])
     datadog_api[key] = {
       'metric': 'trello.card.count',
       'points': 1,
-      'tags': [b, l, a, get_team_lead(a), s]
+      'tags': [b, l, a, lead, s]
     }
 
 def remove_accents(input_str):
