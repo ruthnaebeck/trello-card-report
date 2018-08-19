4763fc5e98c86bb9235912d6259d5f1586bbec54
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-08-19 19:29:51 -0400

    Move zendesk functions

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import secrets
 import trello
 import leads
+from zendesk import find_zendesk_url, get_zendesk_user, get_zendesk_ticket
+
 import requests
 import re
 import json
 import time
 import datetime as dt
+import unicodedata
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
 from httplib2 import Http
-import unicodedata
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(secrets.google_json_file, scope)
@@ -21,54 +23,7 @@ service = build('sheets', 'v4', http=credentials.authorize(Http()))
 wb = gc.open_by_key(secrets.google_sheet_id)
 wks = wb.worksheet('Report')
 
-# Table to organize zendesk ticket assignees by id
-assignees = {}
 datadog_api = {}
-
-def find_zendesk_url(card):
-  try:
-    attach_json = requests.request('GET', trello.url_cards + card['id'] + '/attachments' + trello.tokens).json()
-    if len(attach_json):
-      for a in attach_json:
-        if a['url'].startswith('https://datadog.zendesk.com/agent/tickets/'):
-          return a['url']
-    regex = r'https:\/\/datadog\.zendesk\.com\/agent\/tickets\/[0-9]*'
-    card_str = json.dumps(card)
-    searchObj = re.search(regex, card_str, re.M|re.I)
-    if searchObj:
-      return searchObj.group()
-    else:
-      return 'No Zendesk Ticket Found'
-  except:
-    return 'find_zendesk_url error'
-
-def get_zendesk_user(user_id):
-  try:
-    get_ticket_url = 'https://datadog.zendesk.com/api/v2/users/%s.json' % (user_id)
-    user_json = requests.get(url=get_ticket_url, auth=(secrets.zendesk_email, secrets.zendesk_password)).json()['user']
-    return user_json['name']
-  except:
-    return 'get_zendesk_user error'
-
-def get_zendesk_ticket(ticket_id):
-  try:
-    get_ticket_url = 'https://datadog.zendesk.com/api/v2/tickets/%s.json' % (ticket_id)
-    ticket_json = requests.get(url=get_ticket_url, auth=(secrets.zendesk_email, secrets.zendesk_password)).json()['ticket']
-    assignee_id = ticket_json['assignee_id']
-    try:
-      assignee_name = assignees[assignee_id]
-    except KeyError:
-      assignee_name = get_zendesk_user(assignee_id)
-      assignees[assignee_id] = assignee_name
-    return {
-      'zAssigneeId': assignee_id,
-      'zAssigneeName': assignee_name,
-      'zStatus': ticket_json['status'],
-      'zLastUpdated': ticket_json['updated_at']
-    }
-  except:
-    print('get_zendesk_ticket error')
-    return {}
 
 def numberToLetters(q):
   q = q - 1
@@ -197,7 +152,6 @@ def main_script():
   # new_wks = wb.worksheet('Report ' + today)
   # update_sheet(new_wks, table)
   # print(table)
-  # print(assignees)
   print(datadog_api)
   print('Complete!')
 
---
+++ zendesk.py
@@ -0,0 +1,53 @@
+import secrets
+import trello
+import json
+import requests
+import re
+
+# Table to organize zendesk ticket assignees by id
+assignees = {}
+
+def find_zendesk_url(card):
+  try:
+    attach_json = requests.request('GET', trello.url_cards + card['id'] + '/attachments' + trello.tokens).json()
+    if len(attach_json):
+      for a in attach_json:
+        if a['url'].startswith('https://datadog.zendesk.com/agent/tickets/'):
+          return a['url']
+    regex = r'https:\/\/datadog\.zendesk\.com\/agent\/tickets\/[0-9]*'
+    card_str = json.dumps(card)
+    searchObj = re.search(regex, card_str, re.M|re.I)
+    if searchObj:
+      return searchObj.group()
+    else:
+      return 'No Zendesk Ticket Found'
+  except:
+    return 'find_zendesk_url error'
+
+def get_zendesk_user(user_id):
+  try:
+    get_ticket_url = 'https://datadog.zendesk.com/api/v2/users/%s.json' % (user_id)
+    user_json = requests.get(url=get_ticket_url, auth=(secrets.zendesk_email, secrets.zendesk_password)).json()['user']
+    return user_json['name']
+  except:
+    return 'get_zendesk_user error'
+
+def get_zendesk_ticket(ticket_id):
+  try:
+    get_ticket_url = 'https://datadog.zendesk.com/api/v2/tickets/%s.json' % (ticket_id)
+    ticket_json = requests.get(url=get_ticket_url, auth=(secrets.zendesk_email, secrets.zendesk_password)).json()['ticket']
+    assignee_id = ticket_json['assignee_id']
+    try:
+      assignee_name = assignees[assignee_id]
+    except KeyError:
+      assignee_name = get_zendesk_user(assignee_id)
+      assignees[assignee_id] = assignee_name
+    return {
+      'zAssigneeId': assignee_id,
+      'zAssigneeName': assignee_name,
+      'zStatus': ticket_json['status'],
+      'zLastUpdated': ticket_json['updated_at']
+    }
+  except:
+    print('get_zendesk_ticket error')
+    return {}
