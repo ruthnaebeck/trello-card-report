21abd0bf010cbe491d082caea12317649aca6137
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-09-16 14:25:32 -0400

    diff_in_seconds added

--- main.py
+++ main.py
@@ -34,18 +34,18 @@ initialize(**options)
 
 def main_script():
   # Duplicate the Report worksheet
-  today = dt.datetime.today().strftime('%m-%d-%y')
-  DATA = {'requests': [
-    {
-        'duplicateSheet': {
-            'sourceSheetId': int(wks.id),
-            'insertSheetIndex': 0,
-            'newSheetName': 'Report ' + today
-        }
-    }
-  ]}
-  service.spreadsheets().batchUpdate(
-        spreadsheetId=secrets.google_sheet_id, body=DATA).execute()
+  # today = dt.datetime.today().strftime('%m-%d-%y')
+  # DATA = {'requests': [
+  #   {
+  #       'duplicateSheet': {
+  #           'sourceSheetId': int(wks.id),
+  #           'insertSheetIndex': 0,
+  #           'newSheetName': 'Report ' + today
+  #       }
+  #   }
+  # ]}
+  # service.spreadsheets().batchUpdate(
+  #       spreadsheetId=secrets.google_sheet_id, body=DATA).execute()
 
   # Collect Trello Card / Zendesk ticket info
   table = []
@@ -62,12 +62,13 @@ def main_script():
         print(str(x) + ' - ' + c['name'])
         zendesk_url = find_zendesk_url(c)
         zendesk_id = zendesk_url.split('/').pop()
+        trello_updated = dt.datetime.strptime(c['dateLastActivity'], '%Y-%m-%dT%H:%M:%S.%fZ')
         new_row = [
           b['name'],
           l['name'],
           c['name'],
           c['shortUrl'],
-          str(dt.datetime.strptime(c['dateLastActivity'], '%Y-%m-%dT%H:%M:%S.%fZ')),
+          str(trello_updated),
           zendesk_url
         ]
         if len(zendesk_id) <= 6:
@@ -76,13 +77,16 @@ def main_script():
             agent = zendesk_ticket['zAssigneeName']
             status = zendesk_ticket['zStatus']
             hold_pending = status == 'hold' or status == 'pending'
-            if c['dateLastActivity'] > zendesk_ticket['zLastUpdated'] and hold_pending:
+            zendesk_updated = dt.datetime.strptime(zendesk_ticket['zLastUpdated'], '%Y-%m-%dT%H:%M:%SZ')
+            diff_in_seconds = (trello_updated - zendesk_updated).total_seconds()
+            if diff_in_seconds > 3599 and hold_pending:
+              print('***FLAGGED***')
               # status = u're-opened'
               status = u'flagged'
               tickets_to_open.append(zendesk_id)
             if(isinstance(agent, str)):
               agent = u'unknown'
-            new_row.extend((agent, status, str(dt.datetime.strptime(zendesk_ticket['zLastUpdated'], '%Y-%m-%dT%H:%M:%SZ'))))
+            new_row.extend((agent, status, str(zendesk_updated)))
             add_to_datadog_api(b['tag'], l['tag'], 'zendesk_agent:' + remove_accents(agent.replace(' ', '_').lower()), 'zendesk_status:' + status)
           except KeyError:
             new_row.extend(('unknown', 'error', 'x'))
@@ -96,8 +100,8 @@ def main_script():
       print('--------------------')
 
   # Add card / ticket info to the newly created worksheet
-  new_wks = wb.worksheet('Report ' + today)
-  update_sheet(new_wks, table)
+  # new_wks = wb.worksheet('Report ' + today)
+  # update_sheet(new_wks, table)
 
   # Add metrics through datadog api
   # metrics = []
